14d8a5446c7ca10e44e27d7b8bf1bad8ac9ee1f4
zachkarol430 <zachkarol430@gmail.com>
2021-12-03 22:23:37 -0500

    movie add

---
+++ Selenium_functions.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+from selenium.webdriver.common.proxy import Proxy,ProxyType
+
+
+# proxy_list=[]
+#
+# proxy_ip_port= "fe80::aede:48ff:.49159"
+# proxy=Proxy()
+# proxy.proxy_type= ProxyType.MANUAL
+# proxy.http_proxy=proxy_ip_port
+# proxy.ssl_proxy=proxy_ip_port
+#
+# capabilities = webdriver.DesiredCapabilities.CHROME
+# proxy.add_to_capabilities(capabilities)
+
+
+
+#ADD DESIRED CAPBILITY IF WANT TO USE PROXY AGAIN> ADD TO DRIBER IN GET URL
+
+path= "/Users/zachkarol/Downloads/chromedriver 2"
+s=Service(path)
+
+from selenium import webdriver
+
+
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+
+# you can also import SoftwareEngine, HardwareType, SoftwareType, Popularity from random_user_agent.params
+# you can also set number of user agents required by providing `limit` as parameter
+
+chrome_options = webdriver.ChromeOptions()
+
+
+
+class requester:
+    def get_user_agent(self):
+        software_names = [SoftwareName.CHROME.value]
+        operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value,OperatingSystem.MACOS.value]
+        user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+        user_agents = user_agent_rotator.get_user_agents()
+        user_agent = user_agent_rotator.get_random_user_agent()
+        return user_agent
+    def add_options(self):
+        user_agent = self.get_user_agent()
+        chrome_options.add_argument("--incognito")
+        # chrome_options.add_experimental_option("detach", True)
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument(f"user-agent={user_agent}")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+    def get_url(self,url):
+        self.add_options()
+        driver = webdriver.Chrome(service=s, options=chrome_options)##add desired capbilites for proxt
+        driver.get(url)
+        return driver
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,178 @@
+import gspread
+from bs4 import BeautifulSoup
+import requests
+import time
+import json
+import pygsheets
+from google.oauth2 import service_account
+from oauth2client.service_account import ServiceAccountCredentials
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from Selenium_functions import requester
+
+
+obj=requester()
+
+
+
+#you need to figure out how to integrate class and def functions to find stuff.
+
+
+
+
+
+scope = ["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+
+with open("/Users/zachkarol/Movie_project/movie-project-330118-9bf05ce7085e.json") as source:
+    info = json.load(source)
+creds = service_account.Credentials.from_service_account_info(info)
+
+# creds= ServiceAccountCredentials.from_json_keyfile_name("/Users/zachkarol/Movie_project/movie-project-330118-9bf05ce7085e.json", scope)
+client=pygsheets.authorize(service_account_file="/Users/zachkarol/Movie_project/movie-project-330118-9bf05ce7085e.json")
+
+
+
+sheet=client.open("Movie_proj").sheet1
+
+
+
+movies_list =list(sheet.get_col(1))[1:]
+while("" in movies_list):
+    movies_list.remove("")
+
+#
+#
+
+
+
+class movie_get:
+    def __init__(self, movie):
+        self.movie= movie
+    def get_actor(self):
+        try:
+            try:
+                driver=obj.get_url("https://www.google.com/search?q=" + str(self.movie)+ " cast")
+                element = driver.find_elements(By.CLASS_NAME, "JjtOHd")
+                element_text = element[0].text
+                return element_text
+            except:
+                page = requests.get("https://www.google.com/search?q=" + str(self.movie)+" cast")
+                soup = BeautifulSoup(page.content, 'lxml')
+                actor=soup.find("div", class_="BNeawe s3v9rd AP7Wnd").text
+                return actor
+        except:
+            return "Na"
+    def get_director(self):
+        try:
+            driver = obj.get_url("https://www.google.com/search?q=" + str(self.movie) + " director")
+            element = driver.find_element(By.CLASS_NAME, "FLP8od")
+            element_text = element.text
+            return element_text
+        except:
+            try:
+                page = requests.get("https://www.google.com/search?q=" + str(self.movie) + "director")
+                soup = BeautifulSoup(page.content, 'html.parser')
+                directors = soup.find_all("div", class_="BNeawe deIvCb AP7Wnd")
+                director = directors[1].text
+                if director != "Images":
+                    return director
+                else:
+                    lol
+            except:
+                try:
+                    director = soup.find("div", class_="BNeawe iBp4i AP7Wnd").text
+                    return director
+                except:
+                    return "Na"
+        return "Na"
+    def get_genre(self):
+        try:
+            driver = obj.get_url("https://www.google.com/search?q=" + str(self.movie) + " genre")
+            driver.implicitly_wait(10)
+            element = driver.find_elements(By.CLASS_NAME, "bVj5Zb.FozYP")
+            primary_genre = element[0].text
+            secondary_genre= element[1].text
+            return primary_genre,secondary_genre
+        except:
+            return "Na","Na"
+    def get_box_office(self):
+        try:
+            movie = ("+".join(self.movie.split(" ")))
+            driver = obj.get_url(f"https://www.boxofficemojo.com/search/?q={movie}")
+            url = driver.find_element(By.XPATH, ".//a[@class='a-size-medium a-link-normal a-text-bold']").get_attribute(
+                "href")
+            driver = obj.get_url(url)
+            return driver.find_elements(By.CLASS_NAME, "money")[2].text
+        except:
+            return "Na"
+
+
+#
+#
+# director_list=[]
+# actor_list=[]
+# primary_genre_list=[]
+# secondary_genre_list=[]
+#
+#
+# #just add things you want to pretty simple. I think this sucks but small issue to add data at end.
+#
+# #maybe try parrell requests
+#
+# #maybe add row by time
+# # movie=movie_get(movies_list[-1])
+# #
+# # primary,secondary=movie.get_genre()
+# # sheet.update_row(len(movies_list)+1,[movie.get_actor(),movie.get_director(),primary,secondary,movie.get_box_office()], col_offset=5)
+# box_office_list=[]
+# for i in movies_list:
+#     movie=movie_get(i)
+#     box_office_list.append(movie.get_box_office())
+#
+# sheet.update_col(10,box_office_list, row_offset=1)
+#
+#
+#
+#
+#     # genre=movie.get_genre()
+#     # primary_genre_list.append(genre[0])
+#     # secondary_genre_list.append(genre[1])
+#
+#
+#
+# for i in range(len(director_list)):
+#     if director_list[i]== "":
+#         director_list[i]= "NA"
+#     else:
+#         pass
+#
+#
+# index_na=[i for i, e in enumerate(director_list) if e =="NA"]
+# c = [ movies_list[i] for i in index_na]
+#
+# new_na_list=[]
+# #
+# for i in range(len(c)):
+#     movie=movie_get(c[i])
+#     director=movie.get_director()
+#     new_na_list.append(director)
+#
+# index=0
+# for i in index_na:
+#     director_list[i]=new_na_list[index]
+#     index=index+1
+#
+# list_of_attrib=director_list,actor_list,primary_genre_list, secondary_genre_list
+#
+#
+# #LOOOKO segfs
+# for i in list_of_attrib:
+#     print(i)
+#     #conditional format
+#     # https: // pygsheets.readthedocs.io / en / stable / chart.html
+#     # wks.add_conditional_formatting('A1', 'A4', 'NUMBER_BETWEEN', {'backgroundColor': {'red': 1}}, ['1', '5'])
+#     # sheet.update_col(index,i)
+#     index=index+1
+#
