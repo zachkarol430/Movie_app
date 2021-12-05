30862ce620890f9b07af6c34b7634e549156bdf2
zachkarol430 <zachkarol430@gmail.com>
2021-12-05 01:03:44 -0500

    new commit

--- Selenium_functions.py
+++ Selenium_functions.py
@@ -58,7 +58,7 @@ class requester:
         chrome_options.add_argument("window-size=1400,900")
         chrome_options.add_argument(f"user-agent={user_agent}")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.addArguments("--no-sandbox")
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.add_argument("--disable-setuid-sandbox")
     def get_url(self,url):
