288d9f6962cc675c1f4fabf8c50dadeb19b22cc0
zachkarol430 <zachkarol430@gmail.com>
2021-12-26 23:29:23 -0500

    rsg

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ class movie_get:
         list_foo = [self.get_director, self.get_actor, self.get_box_office]
         with concurrent.futures.ProcessPoolExecutor() as executor:
             thread = [executor.submit(foo) for foo in list_foo]
-        result = [str(x) for x in thread]
+        result = [x.result(timeout=3) for x in thread]
         self.director = result[0]
         self.actor = result[1]
         self.box_office = result[2]
